5e9d5aeb6ac9bb95952e9ed51625818b713ea5b2
sowndar18 <sowndar1802@gmail.com>
2018-12-15 18:32:30 +0530

    Spring basic

---
+++ SpringBasic/src/main/java/com/bridgeit/autowired/Heart.java
@@ -0,0 +1,17 @@
+package com.bridgeit.autowired;
+
+public class Heart {
+	public String humanname;
+	
+public String getHumanname() {
+		return humanname;
+	}
+
+public void setHumanheartobject(String humanheartobject) {
+		this.humanname = humanheartobject;
+	}
+
+void pump() {
+	System.out.println("heart is pumping");
+}
+}
